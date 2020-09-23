4666f187035c50c2b8966f75000d88fbf53e2a6e
Marial Malabag <mjm751@scarletmail.rutgers.edum>
2020-09-23 12:27:35 -0400

    Data

--- Subsetting to USA data.R
+++ Subsetting to USA data.R
@@ -14,28 +14,28 @@ library(tidyverse)
 library(dplyr)
 
 #read in data
-mtdna_papers <- read.csv("mtdna_assembled.csv", stringsAsFactors = FALSE) #read in 
-msat_papers <- read.csv("msat_assembled.csv", stringsAsFactors = FALSE) #read in 
+mtdna_papers <- read.csv("mtdna_assembled_2.csv", stringsAsFactors = FALSE) #read in 
+msat_papers <- read.csv("msat_assembled_2.csv", stringsAsFactors = FALSE) #read in 
 
 ##########################################################################################################################################
 
 ############### Isolating sites to only Continental USA ############### 
 
-### Subset data by latitude and longitude columns in both data sets ###
+#### Subset data by latitude and longitude columns in both data sets ####
 
 #Subset mtdna data
 colnames(mtdna_papers) #get column names to input for the following line of code
 
 mtdna_papers_latlon <- mtdna_papers %>% 
   select(X, spp, CommonName, Source, Country, Site, lat, lon, stockid, CollectionYear, MarkerName, n, bp, He, Hese, Pi, Pise, file) %>%
-  filter(between(lat, 25, 49) & between(lon, -127, -66)) #subset data between the given latitudes and longitudes
+  filter(between(lat, 23, 50) & between(lon, -128, -65), .preserve = TRUE) #subset data between the given latitudes and longitudes
 
 #Subset msat data
 colnames(msat_papers) #get column names to input for the following line of code
 
 msat_papers_latlon <- msat_papers %>% 
   select(X, spp, CommonName, Source, PrimerNote, Country, Site, lat, lon, stockid, CollectionYear, NumMarkers, MarkerName, CrossSpp, n, Repeat, He, Hese, file) %>%
-  filter(between(lat, 25, 49) & between(lon, -127, -66)) #subset data between the given latitudes and longitudes
+  filter(between(lat, 23, 50) & between(lon, -128, -65), .preserve = TRUE) #subset data between the given latitudes and longitudes
 
 ### Subset data further by Country name in both sets ###
 
@@ -44,16 +44,51 @@ unique(mtdna_papers_latlon$Country ) #find unique values to allow for the subset
 
 mtdna_papers_USA <- mtdna_papers_latlon  %>% 
   select(X, spp, CommonName, Source, Country, Site, lat, lon, stockid, CollectionYear, MarkerName, n, bp, He, Hese, Pi, Pise, file) %>%
-  filter(Country == c("United States", "USA", "USA, Western North Atlantic")) #subset data for just continental USA
+  filter(Country %in% c("United States", "USA", "USA, Western North Atlantic"), .preserve = TRUE) #subset data for just continental USA
 
+# , "Northwestern Pacific Ocean ")
 #Subset msat data
 unique(msat_papers_latlon$Country ) #find unique values to allow for the subsetting to just continental USA
 
 msat_papers_USA <- msat_papers_latlon %>% 
   select(X, spp, CommonName, Source, PrimerNote, Country, Site, lat, lon, stockid, CollectionYear, NumMarkers, MarkerName, CrossSpp, n, Repeat, He, Hese, file) %>%
-  filter(Country %in% c("USA", "United States")) #subset data for just continental USA
+  filter(Country %in% c("USA", "United States"), .preserve = TRUE) #subset data for just continental USA
 
-### Write subsetted data as a CSV file ###
+### Double check to see if it was subsetted correctly by doing the reverse process ###
+
+### Subset data further by Country name in both sets ###
+
+#Subset mtdna data
+unique(mtdna_papers$Country ) #find unique values to allow for the subsetting to just continental USA
+
+mtdna_papers_latlon2 <- mtdna_papers  %>% 
+  select(X, spp, CommonName, Source, Country, Site, lat, lon, stockid, CollectionYear, MarkerName, n, bp, He, Hese, Pi, Pise, file) %>%
+  filter(Country %in% c("United States", "USA", "USA, Western North Atlantic"), .preserve = TRUE) #subset data for just continental USA
+
+#Subset msat data
+unique(msat_papers$Country ) #find unique values to allow for the subsetting to just continental USA
+
+msat_papers_latlon2 <- msat_papers %>% 
+  select(X, spp, CommonName, Source, PrimerNote, Country, Site, lat, lon, stockid, CollectionYear, NumMarkers, MarkerName, CrossSpp, n, Repeat, He, Hese, file) %>%
+  filter(Country %in% c("USA", "United States"), .preserve = TRUE) #subset data for just continental USA
+
+### Subset data by latitude and longitude columns in both data sets ###
+
+#Subset mtdna data
+colnames(mtdna_papers_latlon2) #get column names to input for the following line of code
+
+mtdna_papers_USA2 <- mtdna_papers_latlon2 %>% 
+  select(X, spp, CommonName, Source, Country, Site, lat, lon, stockid, CollectionYear, MarkerName, n, bp, He, Hese, Pi, Pise, file) %>%
+  filter(between(lat, 23, 50) & between(lon, -128, -65), .preserve=TRUE) #subset data between the given latitudes and longitudes
+
+#Subset msat data
+colnames(msat_papers_latlon2) #get column names to input for the following line of code
+
+msat_papers_USA2 <- msat_papers_latlon2 %>% 
+  select(X, spp, CommonName, Source, PrimerNote, Country, Site, lat, lon, stockid, CollectionYear, NumMarkers, MarkerName, CrossSpp, n, Repeat, He, Hese, file) %>%
+  filter(between(lat, 23, 50) & between(lon, -128, -65), .preserve=TRUE) #subset data between the given latitudes and longitudes
+
+#### Write subsetted data as a CSV file ####
 
 write.csv(mtdna_papers_USA,'mtdna_papers_USA.csv') #write csv for mtDNA data
 
