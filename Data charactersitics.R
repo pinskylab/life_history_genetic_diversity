6aeec0454084a220b098bb609484d416d62ac280
Marial Malabag <mjm751@scarletmail.rutgers.edum>
2023-03-07 13:36:37 -0500

    New script

---
+++ Data charactersitics.R
@@ -0,0 +1,126 @@
+################################################### Script for dataset ########################################################
+
+#look at dataset and see differnet characteristics (ex: how many hermaphrodites in study)
+
+##########################################################################################################################################
+
+######### Set-up #########
+
+#clear environment
+remove(list = ls())
+
+#load libraries
+library(tidyverse)
+library(dplyr)
+library(lme4)
+library(Hmisc)
+library(ggplot2)
+library(MuMIn)
+library(lmerTest)
+library(glmmTMB)
+library(tidyr)
+library(effects)
+library(sjPlot)
+
+#read in data
+mtdna_data <- read.csv("new_mtdna_full_US_data.csv", stringsAsFactors = FALSE) #read in 
+msat_data <- read.csv("new_msat_full_US_data.csv", stringsAsFactors = FALSE) #read in 
+
+#Fixed Variables for mtDNA He & Pi: Body length/maxlength, fecundity mean, fertilization method, reproduction mode, Bp scale
+#Random Variables for mtDNA He & Pi: Species, source
+#Fixed Variables for msat He: Body length/maxlength, fecundity mean, fertilization method, reproduction mode, CrossSpp
+#Random Variables for msat He: Species, source, ID
+#in brood or similar structure --> internal
+
+##########################################################################################################################################
+
+############# MTDNA JUST LOOKING @ HD #############
+
+#Number of Hd in mtDNA
+mtDNA_He <- mtdna_data %>% drop_na(He) #138 obs.
+
+#Number of repro in mtDNA Hd
+mtDNA_He_repro <- mtDNA_He %>% drop_na(reproductionmode) 
+table (mtDNA_He_repro$reproductionmode) #dioecism = 115 | protogyny = 17
+
+#Number of fert in mtDNA Hd
+mtDNA_He_fert <- mtDNA_He %>% drop_na(fertilization)
+table (mtDNA_He_fert$fertilization) #external = 96 | internal (including in brood) = 36
+
+#Number of sources
+unique(mtDNA_He$Source) #34 sources
+
+#Number of spps
+unique(mtDNA_He$spp) #35 spps
+
+#Range of fecundity
+mtDNA_He[mtDNA_He == "NaN"] <- NA
+range_fec_mtdnaHe <- max(mtDNA_He$fecundity_mean, na.rm =TRUE) - min(mtDNA_He$fecundity_mean, na.rm =TRUE) #5999999 (max = 6e+06, min = 1)
+
+#Range of max length
+range_maxlength_mtdnaHe <- max(mtDNA_He$maxlength, na.rm =TRUE) - min(mtDNA_He$maxlength, na.rm =TRUE) #476.3 (max = 430, min = 5.7)
+
+############# MTDNA JUST LOOKING @ PI #############
+
+#Number of repro in mtDNA Pi
+mtDNA_Pi_repro <- mtdna_data %>% drop_na(reproductionmode) 
+table (mtDNA_Pi_repro$reproductionmode) #dioecism = 134 | protogyny = 17
+
+#Number of fert in mtDNA Pi
+mtDNA_Pi_fert <- mtDNA_He %>% drop_na(fertilization)
+table (mtDNA_Pi_fert$fertilization) #external = 96 | internal (including in brood) = 36
+
+#Number of sources
+unique(mtdna_data$Source) #40 sources
+
+#Number of spps
+unique(mtdna_data$spp) #40 spps
+
+#Range of fecundity
+mtdna_data[mtdna_data == "NaN"] <- NA
+range_fec_mtdnaPi <- max(mtdna_data$fecundity_mean, na.rm =TRUE) - min(mtdna_data$fecundity_mean, na.rm =TRUE) #5999999(max = 6e+06, min = 1)
+
+#Range of max length
+range_maxlength_mtdnaPi <- max(mtdna_data$maxlength, na.rm =TRUE) - min(mtdna_data$maxlength, na.rm =TRUE) #445 (max = 455, min = 10)
+
+############# MSAT #############
+
+#Number of repro in msat
+msat_repro <- msat_data %>% drop_na(reproductionmode) 
+table (msat_repro$reproductionmode) #dioecism = 2786 | protogyny = 248
+
+#Number of fert in msat
+msat_fert <- msat_data %>% drop_na(fertilization)
+table (msat_fert$fertilization) #external = 2026 | internal (including in brood) = 1014
+
+#Number of sources
+unique(msat_data$Source) #40 sources
+
+#Number of spps
+unique(msat_data$spp) #40 spps
+
+#Number of crossspp
+table(msat_data$CrossSpp) #0 = 1593 | 0.25 = 80 | 0.6 = 96 | 1 = 1376
+
+#Range of fecundity
+msat_data[msat_data == "NaN"] <- NA
+range_fec_msat <- max(msat_data$fecundity_mean, na.rm =TRUE) - min(msat_data$fecundity_mean, na.rm =TRUE) #4655806 (max = 4655806, min = 1)
+
+#Range of max length
+range_maxlength_msat <- max(msat_data$maxlength, na.rm =TRUE) - min(msat_data$maxlength, na.rm =TRUE) #476.3 (max = 482, min = 5.7)
+
+############# MSAT & MTDNA (full mtdna dataset) #############
+
+#Common studies ?
+msatsource <- unique(msat_data$Source)
+mtdnasource <- unique(mtdna_data$Source)
+reduce(intersect, list(msatsource, mtdnasource))
+
+tst <- c(unique(msat_data$Source), unique(mtdna_data$Source))
+tst <- tst[duplicated(tst)]
+tst[duplicated(tst)]
+
+#Common spps ?
+tst <- c(unique(msat_data$spp), unique(mtdna_data$spp))
+tst <- tst[duplicated(tst)]
+tst[duplicated(tst)]
